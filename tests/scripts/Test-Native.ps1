ce6101398e133ff31e5afe46af1079c47f1a1f68
Artur <arturcic@gmail.com>
2021-07-18 10:26:12 +0200

    wip - check the path exists

--- tests/scripts/Test-Native.ps1
+++ tests/scripts/Test-Native.ps1
@@ -3,4 +3,9 @@ param(
     [parameter(Mandatory=$true, Position=1)][string] $repoPath
 )
 
+if (Test-Path /native/linux-x64/gitversion -PathType Leaf) {
+    Write-Host "it exists"
+} else {
+    { Write-Host "it does not exist" }
+}
 & "/native/linux-x64/gitversion" /repo /showvariable FullSemver;
