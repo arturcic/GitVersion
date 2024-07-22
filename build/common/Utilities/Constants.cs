23bad24699d8a9649cdd31a0566d2db2f97008da
Artur Stolear <artur.stolear@gmail.com>
2024-07-22 10:09:01 +0200

    Sunset .net 6.0

--- build/common/Utilities/Constants.cs
+++ build/common/Utilities/Constants.cs
@@ -6,9 +6,8 @@ public static class Constants
     public const string RepoOwner = "GitTools";
     public const string Repository = "GitVersion";
 
-    public const string VersionCurrent = "6.0";
     public const string VersionLatest = "8.0";
-    public static readonly string[] Frameworks = [VersionCurrent, VersionLatest];
+    public static readonly string[] Frameworks = [VersionLatest];
 
     public const string DefaultBranch = "main";
     public const string DefaultConfiguration = "Release";
